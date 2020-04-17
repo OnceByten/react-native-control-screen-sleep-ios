a0a2774ca9daee8ac6fe805125d7834d3c4d6092
Simon Gough <simon@oncebyten.com>
2020-04-17 10:46:14 +0100

    use main thread

--- RNControlScreenSleep/RNControlScreenSleep/RNControlScreenSleep.m
+++ RNControlScreenSleep/RNControlScreenSleep/RNControlScreenSleep.m
@@ -17,108 +17,110 @@ RCT_EXPORT_METHOD(turnSleep:(NSString *)sleepDesc
                   callback:(RCTResponseSenderBlock)successCallback) {
     
     if ([sleepDesc isEqual: @"sleepOff"]) {
-        
-        // Acquire a reference to the local UIApplication singleton
-        UIApplication* app = [UIApplication sharedApplication];
-        
-        // If the timer is not disbled, prevent sleep
-        if (![app isIdleTimerDisabled]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // Acquire a reference to the local UIApplication singleton
+            UIApplication* app = [UIApplication sharedApplication];
             
-            [app setIdleTimerDisabled:true];
-            
-            // Show success message that screen has been put to sleep
-            NSDictionary *resultsDict = @{
-                                          @"success" : @YES,
-                                          @"successMsg"  : @"App is now in keep awake mode"
-                                          };
-            
-            // Call the JavaScript sucess handler
-            successCallback(@[resultsDict]);
-            return;
-            
-        } else {
-            
-            // Show failure message
-            NSDictionary *resultsDict = @{
-                                          @"success" : @NO,
-                                          @"errMsg"  : @"App is already in keep awake mode."
-                                          };
-            
-            // Execute the JavaScript failure callback handler
-            failureCallback(@[resultsDict]);
-            return;
-            
-        }
-        
+            // If the timer is not disbled, prevent sleep
+            if (![app isIdleTimerDisabled]) {
+                
+                [app setIdleTimerDisabled:true];
+                
+                // Show success message that screen has been put to sleep
+                NSDictionary *resultsDict = @{
+                                              @"success" : @YES,
+                                              @"successMsg"  : @"App is now in keep awake mode"
+                                              };
+                
+                // Call the JavaScript sucess handler
+                successCallback(@[resultsDict]);
+                return;
+                
+            } else {
+                
+                // Show failure message
+                NSDictionary *resultsDict = @{
+                                              @"success" : @NO,
+                                              @"errMsg"  : @"App is already in keep awake mode."
+                                              };
+                
+                // Execute the JavaScript failure callback handler
+                failureCallback(@[resultsDict]);
+                return;
+                
+            }
+        });
     } else if ([sleepDesc isEqual: @"sleepOn"]) {
         
-        // Acquire a reference to the local UIApplication singleton
-        UIApplication* app = [UIApplication sharedApplication];
-        
-        // If the timer is disbled, turn sleep mode back on
-        if ([app isIdleTimerDisabled]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // Acquire a reference to the local UIApplication singleton
+            UIApplication* app = [UIApplication sharedApplication];
             
-            [app setIdleTimerDisabled:false];
-            
-            // Show success message that screen is now in sleep mode
-            NSDictionary *resultsDict = @{
-                                          @"success" : @YES,
-                                          @"successMsg"  : @"App will now be able to sleep."
-                                          };
-            
-            // Call the JavaScript sucess handler
-            successCallback(@[resultsDict]);
-            return;
-            
-        } else {
-            
-            // Show failure message
-            NSDictionary *resultsDict = @{
-                                          @"success" : @NO,
-                                          @"errMsg"  : @"App is already able to sleep."
-                                          };
-            
-            // Execute the JavaScript failure callback handler
-            failureCallback(@[resultsDict]);
-            return;
-            
-        }
+            // If the timer is disbled, turn sleep mode back on
+            if ([app isIdleTimerDisabled]) {
+                
+                [app setIdleTimerDisabled:false];
+                
+                // Show success message that screen is now in sleep mode
+                NSDictionary *resultsDict = @{
+                                              @"success" : @YES,
+                                              @"successMsg"  : @"App will now be able to sleep."
+                                              };
+                
+                // Call the JavaScript sucess handler
+                successCallback(@[resultsDict]);
+                return;
+                
+            } else {
+                
+                // Show failure message
+                NSDictionary *resultsDict = @{
+                                              @"success" : @NO,
+                                              @"errMsg"  : @"App is already able to sleep."
+                                              };
+                
+                // Execute the JavaScript failure callback handler
+                failureCallback(@[resultsDict]);
+                return;
+                
+            }
+        });
         
     } else if ([sleepDesc isEqual: @"sleepToggle"]) {
-        
-        // Acquire a reference to the local UIApplication singleton
-        UIApplication* app = [UIApplication sharedApplication];
-        
-        // If the timer is disbled, turn sleep mode back on
-        if ([app isIdleTimerDisabled]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // Acquire a reference to the local UIApplication singleton
+            UIApplication* app = [UIApplication sharedApplication];
             
-            [app setIdleTimerDisabled:false];
-            
-            // Show success message that screen is now in sleep mode
-            NSDictionary *resultsDict = @{
-                                          @"success" : @YES,
-                                          @"successMsg"  : @"App is now able to go to sleep."
-                                          };
-            
-            // Call the JavaScript sucess handler
-            successCallback(@[resultsDict]);
-            return;
-            
-        } else {
-            
-            [app setIdleTimerDisabled:true];
-            
-            // Show success message that screen has been put to sleep
-            NSDictionary *resultsDict = @{
-                                          @"success" : @YES,
-                                          @"successMsg"  : @"App is now in keep awake mode."
-                                          };
-            
-            // Call the JavaScript sucess handler
-            successCallback(@[resultsDict]);
-            return;
-        }
-        
+            // If the timer is disbled, turn sleep mode back on
+            if ([app isIdleTimerDisabled]) {
+                
+                [app setIdleTimerDisabled:false];
+                
+                // Show success message that screen is now in sleep mode
+                NSDictionary *resultsDict = @{
+                                              @"success" : @YES,
+                                              @"successMsg"  : @"App is now able to go to sleep."
+                                              };
+                
+                // Call the JavaScript sucess handler
+                successCallback(@[resultsDict]);
+                return;
+                
+            } else {
+                
+                [app setIdleTimerDisabled:true];
+                
+                // Show success message that screen has been put to sleep
+                NSDictionary *resultsDict = @{
+                                              @"success" : @YES,
+                                              @"successMsg"  : @"App is now in keep awake mode."
+                                              };
+                
+                // Call the JavaScript sucess handler
+                successCallback(@[resultsDict]);
+                return;
+            }
+        });
     } else {
         
         // Show failure message
